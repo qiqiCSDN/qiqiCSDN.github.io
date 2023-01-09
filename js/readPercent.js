466f1a1e0bd59c49dae8d143cd37378646b57183
赵文龙 <374623337@qq.com>
2023-01-09 10:50:06 +0800

    Site updated: 2023-01-09 10:50:01

--- js/readPercent.js
+++ js/readPercent.js
@@ -43,17 +43,17 @@ document.addEventListener("copy", function () {
     }, 300);
 })
 
-//爬取提示
-new Vue({
-    data: function () {
-        this.$notify({
-            title: "你已被发现😜",
-            message: "小伙子，扒源记住要遵循GPL协议！",
-            position: 'top-left',
-            offset: 50,
-            showClose: true,
-            type: "warning",
-            duration: 5000
-        });
-    }
-})
+// //爬取提示
+// new Vue({
+//     data: function () {
+//         this.$notify({
+//             title: "你已被发现😜",
+//             message: "小伙子，扒源记住要遵循GPL协议！",
+//             position: 'top-left',
+//             offset: 50,
+//             showClose: true,
+//             type: "warning",
+//             duration: 5000
+//         });
+//     }
+// })
